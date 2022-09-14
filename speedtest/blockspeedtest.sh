cba72f3fae94b28ecda8b3a229b984c5ebcb980e
235nvnybtq235 <113501816+235nvnybtq235@users.noreply.github.com>
2022-09-14 13:58:12 +0700

    Update blockspeedtest.sh

--- speedtest/blockspeedtest.sh
+++ speedtest/blockspeedtest.sh
@@ -1,4 +1,3 @@
-rm -rf blockspeedtest.sh
 clear
 echo "   BLOCK SPEEDTEST BY ADMIN SPEED4G.ME"
 echo "   1. SETUP"
@@ -9,7 +8,7 @@ read -p "  Vui Lòng Nhập : " num
     case "${num}" in
         1) apt-get update && apt install iptables-persistent netfilter-persistent -y && clear && echo "   DONE !"
         ;;
-        2) curl -OL https://speed4g.xyz/speedtest/blockip.sh && bash blockip.sh
+        2) bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/speedtest/blockip.sh)
         ;;
         3) iptables -F && clear && echo "   DONE !"
         ;;
