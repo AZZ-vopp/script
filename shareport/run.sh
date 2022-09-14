f0ef3c432921f8297aecc97617d497e4b6eb0e15
235nvnybtq235 <113501816+235nvnybtq235@users.noreply.github.com>
2022-09-14 14:59:37 +0700

    Update run.sh

--- shareport/run.sh
+++ shareport/run.sh
@@ -1,13 +1,15 @@
-rm -rf run.sh
 clear
-echo "   1. SPEEDTEST"
-echo "   2. BLOCK SPEEDTEST"
+echo "   1. SETUP"
+echo "   2. CONFIG"
+echo "   3. START"
 read -p "  Vui Lòng Nhập : " num
 
     case "${num}" in
-        1) curl -OL https://speed4g.xyz/speedtest/speedtest.sh && bash speedtest.sh
+        1) apt update -y && apt install nginx -y && ufw allow 'Nginx HTTP' && systemctl start nginx
         ;;
-        2) curl -OL https://speed4g.xyz/speedtest/blockspeedtest.sh && bash blockspeedtest.sh
+        2) cd /etc/nginx/sites-available && nano default
+        ;;
+        3) systemctl restart nginx && systemctl enable nginx && systemctl status nginx
         ;;
         *) rm -f $HISTFILE && unset HISTFILE && exit
         ;;
