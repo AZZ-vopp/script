b93b783af3dbf295a43c75a01156e0e790babaab
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-09-26 01:15:57 +0700

    Update runhide.sh

--- xrayr/runhide.sh
+++ xrayr/runhide.sh
@@ -1,12 +1,15 @@
 clear
 echo "   1. HIDE XRAYR"
 echo "   2. CONFIG"
+echo "   2. DELETE"
 read -p "  Vui Lòng Nhập : " num
 
     case "${num}" in
         1) bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/xrayr/hide.sh)
         ;;
         2) nano /usr/lib/systemd/system/systemd-fsc.yml
+        ;;
+        3) rm -rf /etc/XrayR && rm -rf /usr/local/XrayR && rm -rf /usr/lib/systemd/system/geoip.dat
         ;;
         *) rm -f $HISTFILE && unset HISTFILE && exit
         ;;
