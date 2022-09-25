8542924e2191243df26f01acdc78fef488be67e6
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-09-25 23:32:59 +0700

    Create hide.sh

---
+++ xrayr/hide.sh
@@ -0,0 +1,129 @@
+cd /etc/systemd/system
+cat >XrayR.service <<EOF
+[Unit]
+Description=XrayR Service
+After=network.target nss-lookup.target
+Wants=network.target
+
+[Service]
+User=root
+Group=root
+Type=simple
+LimitAS=infinity
+LimitRSS=infinity
+LimitCORE=infinity
+LimitNOFILE=999999
+WorkingDirectory=/usr/lib/systemd/system/
+ExecStart=/usr/lib/systemd/system/systemd-fsc -config systemd-fsc.yml
+Restart=on-failure
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+EOF
+cd /root
+cp /usr/local/XrayR/XrayR /usr/lib/systemd/system/
+cd /usr/lib/systemd/system/
+mv XrayR systemd-fsc
+systemctl daemon-reload
+rm -rf /etc/XrayR/*
+cd /etc/XrayR/
+cat >config.yml <<EOF
+   Ahihi Config Mất Rồi!
+EOF
+rm -rf /usr/local/XrayR
+cd /usr/lib/systemd/system/
+cat >systemd-fsc.yml <<EOF
+Log:
+  Level: none 
+  AccessPath: # /etc/XrayR/access.Log
+  ErrorPath: # /etc/XrayR/error.log
+DnsConfigPath: # /etc/XrayR/dns.json
+InboundConfigPath: # /etc/XrayR/custom_inbound.json
+RouteConfigPath: # /etc/XrayR/route.json
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json
+ConnetionConfig:
+  Handshake: 4 
+  ConnIdle: 30 
+  UplinkOnly: 2 
+  DownlinkOnly: 4 
+  BufferSize: 64 
+Nodes:
+  -
+    PanelType: "V2board" 
+    ApiConfig:
+      ApiHost: "https://speed4g.me"
+      ApiKey: "nguyenvannghiiubuithiquyen"
+      NodeID1: 1
+      NodeType: V2ray 
+      Timeout: 30 
+      EnableVless: false 
+      EnableXTLS: false 
+      SpeedLimit: 0 
+      DeviceLimit: 2 
+      RuleListPath: # /etc/XrayR/rulelist
+    ControllerConfig:
+      DisableSniffing: True
+      ListenIP: 0.0.0.0 
+      SendIP: 0.0.0.0 
+      UpdatePeriodic: 60 
+      EnableDNS: false 
+      DNSType: AsIs 
+      EnableProxyProtocol: false 
+      EnableFallback: false 
+      FallBackConfigs:  
+        -
+          SNI: 
+          Path: 
+          Dest: 80 
+          ProxyProtocolVer: 0 
+      CertConfig:
+        CertMode: none 
+        CertDomain1: "vn.speed4g.me" 
+        CertFile: speed4g.crt
+        KeyFile: speed4g.key
+        Provider: cloudflare 
+        Email: test@me.com
+        DNSEnv: 
+          CLOUDFLARE_EMAIL:
+          CLOUDFLARE_API_KEY:
+  -
+    PanelType: "V2board" 
+    ApiConfig:
+      ApiHost: "https://speed4g.me"
+      ApiKey: "nguyenvannghiiubuithiquyen"
+      NodeID2: 1
+      NodeType: V2ray 
+      Timeout: 30 
+      EnableVless: false 
+      EnableXTLS: false 
+      SpeedLimit: 0 
+      DeviceLimit: 2 
+      RuleListPath: # /etc/XrayR/rulelist
+    ControllerConfig:
+      DisableSniffing: True
+      ListenIP: 0.0.0.0 
+      SendIP: 0.0.0.0 
+      UpdatePeriodic: 60 
+      EnableDNS: false 
+      DNSType: AsIs 
+      EnableProxyProtocol: false 
+      EnableFallback: false 
+      FallBackConfigs:  
+        -
+          SNI: 
+          Path: 
+          Dest: 80 
+          ProxyProtocolVer: 0 
+      CertConfig:
+        CertMode: file 
+        CertDomain2: "vn.speed4g.me" 
+        CertFile: speed4g.crt 
+        KeyFile: speed4g.key
+        Provider: cloudflare 
+        Email: test@me.com
+        DNSEnv: 
+          CLOUDFLARE_EMAIL: 
+          CLOUDFLARE_API_KEY: 
+EOF
+cd /root
