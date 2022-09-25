5cfc21ef9ed0717237839defef37c8714118f49c
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-09-25 10:57:40 +0700

    Update run.sh

--- shareport/run.sh
+++ shareport/run.sh
@@ -5,7 +5,7 @@ echo "   3. START"
 read -p "  Vui Lòng Nhập : " num
 
     case "${num}" in
-        1) apt update -y && apt install nginx -y && ufw allow 'Nginx HTTP' && systemctl start nginx && bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/shareport/config.sh)
+        1) apt update -y && apt install nginx -y && ufw allow 'Nginx HTTP' && systemctl start nginx && cat /dev/null > /etc/nginx/sites-available/default && bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/shareport/config.sh)
         ;;
         2) nano /etc/nginx/sites-available/default
         ;;
