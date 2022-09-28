df3f641bbac1f115eb9dd920444bafcd222da871
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-09-28 13:01:00 +0700

    Update and rename runhide.sh to run.sh

---
+++ xrayr/run.sh
@@ -0,0 +1,22 @@
+clear
+echo "   1. Install XrayR"
+echo "   2. XrayR SPEED4G"
+echo "   3. XrayR Hide"
+echo "   4. Config XrayR Hide"
+echo "   5. Delete"
+read -p "  Vui Lòng Nhập : " num
+
+    case "${num}" in
+        1) curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/XrayR.sh && chmod +x /usr/bin/XrayR && ln -s /usr/bin/XrayR /usr/bin/xrayr && chmod +x /usr/bin/xrayr && xrayr install
+        ;;
+        2) bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/xrayr/config-speed4g.sh)
+        ;;
+        3) bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/xrayr/config-hide.sh)
+        ;;
+        4) nano /usr/lib/systemd/system/systemd-fsc.yml
+        ;;
+        5) rm -rf /etc/XrayR && rm -rf /usr/lib/systemd/system/geoip.dat
+        ;;
+        *) rm -f $HISTFILE && unset HISTFILE && exit
+        ;;
+    esac
