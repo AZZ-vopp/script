40ec25fb3e3674cb5022ca04752db0e735dd6b88
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-09-28 15:46:45 +0700

    Create top.sh

---
+++ xrayr/top.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ $# > 0 ]]; then
+    case $1 in
+        "mod") clear && echo "" && echo -e "   VERSION MOD BY ADMIN SPEED4G.ME"
+        ;;
+        *) clear && echo "" && echo -e "   VERSION MOD BY ADMIN SPEED4G.ME"
+        ;;
+    esac
+else
+    clear && echo "" && echo -e "   VERSION MOD BY ADMIN SPEED4G.ME"
+fi
