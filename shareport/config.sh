d90d9838480a3ff14fe99308d3c4ad1b3081f144
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-10-01 00:05:46 +0700

    Update config.sh

--- shareport/config.sh
+++ shareport/config.sh
@@ -9,17 +9,16 @@ events {
 	worker_connections 65535;
 }
 http {
-  sendfile on;
+  	sendfile on;
 	tcp_nopush on;
 	tcp_nodelay on;
 	keepalive_timeout 65;
 	types_hash_max_size 2048;
 	include /etc/nginx/mime.types;
 	default_type application/octet-stream;
-  ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
+  	ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3;
 	ssl_prefer_server_ciphers on;
-  gzip on;
-	include /etc/nginx/conf.d/*.conf;
+  
 	include /etc/nginx/sites-enabled/*;
 }
 EOF
