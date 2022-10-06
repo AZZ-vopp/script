b67213a41ba0a0ebdce96fdd2b960c6643a4140c
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-10-07 01:07:31 +0700

    Update run.sh

--- crack-aa/run.sh
+++ crack-aa/run.sh
@@ -1,6 +1,6 @@
-cd /www/server/panel/data
+cd /www/server/panel/data/
 sed -i "s|"endtime": -1.*|"endtime": 999999999999|" ./plugin.json
-cd /www/server/panel/class
+cd /www/server/panel/class/
 sed -i "s|softList['list'] = tmpList.*|softList['list'] = tmpList
         softList['pro'] = 1
 for soft in softList['list']:
