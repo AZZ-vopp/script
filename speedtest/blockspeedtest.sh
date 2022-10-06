e66e27e6199323a9470e909f08d1b7d9edf0b834
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-10-06 23:11:05 +0700

    Update blockspeedtest.sh

--- speedtest/blockspeedtest.sh
+++ speedtest/blockspeedtest.sh
@@ -1,16 +1,19 @@
 clear
 echo "   BLOCK SPEEDTEST BY ADMIN SPEED4G.ME"
 echo "   1. SETUP"
-echo "   2. BLOCK"
-echo "   3. UNBLOCK"
+echo "   2. BLOCK QT"
+echo "   3. BLOCK VN"
+echo "   4. UNBLOCK"
 read -p "  Vui Lòng Nhập : " num
 
     case "${num}" in
         1) apt-get update && apt install iptables-persistent netfilter-persistent -y && clear && echo "   DONE !"
         ;;
-        2) bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/speedtest/blockip.sh)
+        2) bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/speedtest/block-qt.sh)
         ;;
-        3) iptables -F && clear && echo "   DONE !"
+        3) bash <(curl -Ls https://raw.githubusercontent.com/235nvnybtq235/script/main/speedtest/block-vn.sh)
+        ;;
+        4) iptables -F && clear && echo "   DONE !"
         ;;
         *) rm -f $HISTFILE && unset HISTFILE && exit
         ;;
