8201e9205684cf5357b3eaabe956638084243e23
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-10-07 01:01:14 +0700

    Update run.sh

---
+++ crack-aa/run.sh
@@ -0,0 +1,9 @@
+cd /www/server/panel/data
+sed -i "s|"endtime": -1.*|"endtime": 999999999999|" ./plugin.json
+cd /www/server/panel/class
+sed -i "s|softList['list'] = tmpList.*|softList['list'] = tmpList
+        softList['pro'] = 1
+for soft in softList['list']:
+    soft['endtime'] = 0|" ./panelplugin.py
+cd /root
+reboot
