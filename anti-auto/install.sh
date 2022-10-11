fc4a3ec87df8bd63f20a0af629aad63905756896
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-10-12 01:17:56 +0700

    Update install.sh

--- anti-auto/install.sh
+++ anti-auto/install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 if ! [ $(id -u) = 0 ]; then
-   echo "only root run installer"
+   echo "Chưa Cấp Quyền Root !"
    exit 1
 fi
 if [ ! -f /vddos/vddos ]; then
-	echo "First VDDOS INSTALL"
+	echo "Chưa Cài Đặt Anti"
 fi
 yum install net-tools
 yum install screen
 cd /root/
-wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot.php
-wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot.sh
-wget https://raw.githubusercontent.com/ertugrulturan/vddos-sensor-plugin/main/bot2.php
-wget https://github.com/ertugrulturan/vddos-sensor-plugin/blob/main/under.sh
+wget https://raw.githubusercontent.com/235nvnybtq235/script/main/anti-auto/bot.php
+wget https://raw.githubusercontent.com/235nvnybtq235/script/main/anti-auto/bot.sh
+wget https://raw.githubusercontent.com/235nvnybtq235/script/main/anti-auto/bot2.php
+wget https://raw.githubusercontent.com/235nvnybtq235/script/main/anti-auto/under.sh
 clear
-echo "Only bot.sh And under.sh Domain Name Edit And Run Script!"
-echo 'Start (Run Command) : $ screen -S "t13r" bash /root/bot.sh'
+
