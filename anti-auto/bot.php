8a2c34f70ebddca55c8f7d593bcce26dbdf21582
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-10-12 16:09:48 +0700

    Update bot.php

--- anti-auto/bot.php
+++ anti-auto/bot.php
@@ -1,10 +1,10 @@
 <?php
 // netstat -tn | awk '{print $5}' | sed -e 's/:.*//' | grep '\.'| sort | uniq -c | sort -nr | head -24
-$verigelen = file_get_contents("/root/speed4g.me/antiddos.txt");
+$verigelen = file_get_contents("/root/antiddos.txt");
 $veritext = base64_decode($verigelen);
 preg_match_all('@     (.*?) @si',$veritext,$obirninja);
 $firewalld = (trim($obirninja[0][0]));
 if ($firewalld > 8) {
-	@exec('screen -S "underattackon" bash /root/speed4g.me/under.sh');
+	@exec('screen -S "underattackon" bash /root/under.sh');
 }
 ?>
