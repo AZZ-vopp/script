b0a8819540af7b950e0771cc625742136d9b59db
Nguyễn Nghị <113501816+235nvnybtq235@users.noreply.github.com>
2022-10-13 12:26:45 +0700

    Update run.sh

--- anti-ddos/run.sh
+++ anti-ddos/run.sh
@@ -1,24 +1,3 @@
-clear
-echo "   1. Install"
-echo "   2. Config"
-echo "   3. Get SSL"
-echo "   4. Restart"
-read -p "  Vui Lòng Nhập : " num
-
-    case "${num}" in
-        1) yum -y install epel-release ; yum -y install wget zip unzip tar curl  && \        
-        yum -y install nano net-tools curl wget gc gcc gcc-c++ pcre-devel zlib-devel make wget openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel gperftools gperftools-devel perl-ExtUtils-Embed  && \
-        yum -y install screen htop iotop iptraf nano net-tools gcc automake libffi-devel zlib zlib-devel gcc gcc-c++ autoconf apr-util-devel gc gcc gcc-c++ pcre-devel zlib-devel make wget openssl openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel gperftools gperftools-devel  perl-ExtUtils-Embed && \
-        yum -y install gnutls-utils sshpass rsync && \
-        yum -y install bind-utils sysstat bc tar curl wget gc gcc gcc-c++ pcre-devel zlib-devel make wget openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel gperftools gperftools-devel perl-ExtUtils-Embed gcc automake autoconf apr-util-devel gc gcc gcc-c++ pcre-devel zlib-devel make wget openssl openssl-devel libxml2-devel libxslt-devel gd-devel perl-ExtUtils-Embed GeoIP-devel gperftools gperftools-devel perl-ExtUtils-Embed perl perl-devel perl-ExtUtils-Embed libxslt libxslt-devel libxml2 libxml2-devel gd gd-devel GeoIP GeoIP-devel gperftools-devel wget yum-utils make gcc openssl-devel bzip2-devel libffi-devel zlib-devel screen htop iotop iptraf nano net-tools gcc automake libffi-devel zlib zlib-devel gcc gcc-c++ autoconf apr-util-devel gc
-        yum -y install libatomic_ops-devel
-        ;;
-        2) nano /vddos/conf.d/website.conf
-        ;;
-        3) openssl req -newkey rsa:2048 -x509 -sha256 -days 365 -nodes -out /root/speed4g.crt -keyout /root/speed4g.key -subj "/C=JP/ST=Tokyo/L=Chiyoda-ku/O=Google Trust Services LLC/CN=google.com"
-        ;;
-        4) vddos restart
-        ;;
-        *) echo "Nhập Lại"
-        ;;
-    esac
+curl -X GET "https://api.cloudflare.com/client/v4/zones/afaff60b62f16380152a695b4e6f5df8/firewall/rules" \
+     -H "X-Auth-Email: speed4g.xyz@gmail.com" \
+     -H "X-Auth-Key: d0e56caee653972fa0d6d785450b8bbcbdca3"
